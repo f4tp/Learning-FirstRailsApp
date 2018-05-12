4473398abe568405695392e9947aca437511d49d
f4tp <mr_treadwell@live.co.uk>
2018-05-12 12:05:18 +0100

    final commit after tutorial done

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+
+  def home()
+    #puts("hello")
+  end
+
+  def about()
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#http URL                controller#method
+get 'welcome/home', to: 'welcome#home'
+get 'welcome/about', to: 'welcome#about'
+
 end
